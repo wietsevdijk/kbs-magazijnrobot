5f27c52eb40821db32e8c7b2fe01e07072173dd1
CarlijnGJ <carlijn.blom@windesheim.nl>
2023-04-25 11:08:20 +0200

    Verdere changes startscherm HMI
    - Grid aangepast van buttons naar panels
    - Panelbackgrounds en borders gefixt

--- RobotGUI/src/com/testfiles/Testscherm.java
+++ RobotGUI/src/com/testfiles/Testscherm.java
@@ -8,6 +8,8 @@ public class Testscherm extends JFrame{
 
     private int[] squareArray = new int[25];
 
+    private Color DarkGreen = new Color(0, 205, 0);
+
     private JPanel links;
     private JPanel rechts;
 
@@ -33,16 +35,34 @@ public class Testscherm extends JFrame{
 
     public void addPanels() {
 
-        //Gooit het linkerpanel erin
+        //Bereidt het linkerpanel voor
         links = new JPanel();
         links.setLayout(new FlowLayout());
+
+        //Maakt de dropdowns en gooit het linkerpanel erin
+
         add(links);
 
-        //Gooit het rechterpanel erin
+        //Initieert de layout voor het rechterpanel
+        GridLayout grLay = new GridLayout(5, 5);
+
+        //Bereidt het rechterpanel voor
         rechts = new JPanel();
-        rechts.setLayout(new GridLayout(5, 5));
+        rechts.setBackground(Color.GRAY);
+
+        //Zet de layout en tussenlijntjes (Voorgeinitieerde layout voor nodig!)
+        rechts.setLayout(grLay);
+        grLay.setHgap(5);
+        grLay.setVgap(5);
+
+        //Maakt de grid en gooit het rechterpanel erin
+        rechts.setBorder(BorderFactory.createLineBorder(Color.gray, 5));
         addGridPanels();
         add(rechts);
+
+    }
+
+    public void addDropdowns() {
 
     }
 
@@ -51,8 +71,18 @@ public class Testscherm extends JFrame{
         for(int i = 0; i < squareArray.length; i++) {
 
             squareArray[i] = i;
+            Panel panel = new Panel();
+            JLabel naam = new JLabel("" + (i+1));
 
-            rechts.add(new JButton("" + (squareArray[i] + 1)));
+            if(i % 2 == 0) {
+                panel.setBackground(Color.green);
+            }
+            else {
+                panel.setBackground(DarkGreen);
+            }
+
+            rechts.add(panel);
+            panel.add(naam);
 
         }
 
