56ed3d4ba61da526ac78eac822259ae155ff0c1d
Wietse <wietsedaanvandijk@hotmail.com>
2023-05-09 14:35:02 +0200

    Lezen encoder op slave-arduino
    Werkt nog niet helemaal, kan niet verschillende kanten op tellen

--- JoystickMotorControl/sketch_arduino2/sketch_arduino2.ino
+++ JoystickMotorControl/sketch_arduino2/sketch_arduino2.ino
@@ -1,14 +1,15 @@
 #include <Wire.h>
-#define Encoder_output_x 5
-#define Encoder_output_y 6
-#define Encoder_output_z 7
+#define Encoder_output_x 2
+//#define Encoder_output_y 6
+//#define Encoder_output_z 7
 
 byte x;
 String command = "";
+
 int Count_pulses = 0;
 
 void setup() {
-  TCCR2B = TCCR2B & B11111000 | B00000111; // for PWM frequency of 30.64 Hz
+  TCCR2B = TCCR2B & B11111000 | B00000111;  // for PWM frequency of 30.64 Hz
 
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -18,54 +19,51 @@ void setup() {
   Wire.onReceive(receiveEvent);
 
   //Set encoders as input
-  pinMode(Encoder_output_x,INPUT); // sets the Encoder_output_x pin as the input
-  pinMode(Encoder_output_y,INPUT); // sets the Encoder_output_y pin as the input
-  pinMode(Encoder_output_z,INPUT); // sets the Encoder_output_z pin as the input
+  pinMode(Encoder_output_x, INPUT);  // sets the Encoder_output_x pin as the input
+  //pinMode(Encoder_output_y, INPUT);  // sets the Encoder_output_y pin as the input
+  //pinMode(Encoder_output_z, INPUT);  // sets the Encoder_output_z pin as the input
+  attachInterrupt(digitalPinToInterrupt(Encoder_output_x), DC_Motor_Encoder, RISING);
 
-    //Setup Channel A
-  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
-  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+  //Setup Channel A
+  pinMode(12, OUTPUT);  //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT);   //Initiates Brake Channel A pin
 }
 
-void receiveEvent(int bytes){
+void receiveEvent(int bytes) {
   command = "";
-  for(int i = 0; i < bytes; i++) {
+  for (int i = 0; i < bytes; i++) {
     command = command + (char)Wire.read();
   }
 
   //x = Wire.read();
-  Serial.println("RECEIVED: " + command);
+  //Serial.println("RECEIVED: " + command);
 }
 
-  void DC_Motor_Encoder(){
-    int b = digitalRead(Encoder_output_x);
-    if(b > 0){
+void DC_Motor_Encoder() {
+  int b = digitalRead(Encoder_output_x);
+  if (b > 0) {
     Count_pulses++;
-  }
-  else{
+  } else {
     Count_pulses--;
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
 
-    if(command.equals("VOOR")){ //STUUR NAAR VOREN
+
+  if (command.equals("VOOR")) {  //STUUR NAAR VOREN
     digitalWrite(12, LOW);
     digitalWrite(9, LOW);
     analogWrite(3, 200);
-    } else if(command.equals("ACHTER")){ //STUUR NAAR ACHTER
+  } else if (command.equals("ACHTER")) {  //STUUR NAAR ACHTER
     digitalWrite(12, HIGH);
     digitalWrite(9, LOW);
     analogWrite(3, 200);
-   } else{
-    digitalWrite(9, HIGH); //ENGAGE BRAKES
+  } else {
+    digitalWrite(9, HIGH);  //ENGAGE BRAKES
     analogWrite(3, 0);
   }
-  
-  Serial.println(Count_pulses);
-    DC_Motor_Encoder();
-}
 
-  
+  Serial.println(Count_pulses);
+}
