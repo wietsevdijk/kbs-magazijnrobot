52cd72eea715c1820ff2ccf9dee1a71e7cb326e6
DaneSchuijt22 <103997161+PEWPEWPEWPEWPEWPEWPEWPEWPEWPEWPEW@users.noreply.github.com>
2023-06-14 16:32:05 +0200

    SerialComm
    Data wordt uitgelezen en verstuurd maar wordt nog ontvangen in delen. Doen met een buffer werkt niet. Krijg dan een timeout error of geen error maar ook geen resultaat

--- RobotGUI/src/com/company/RobotCommands.java
+++ RobotGUI/src/com/company/RobotCommands.java
@@ -1,29 +1,114 @@
 package com.company;
 
 import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.mysql.cj.util.StringUtils;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
 
 
-public class RobotCommands {
-    private SerialPort sp;
+public class RobotCommands implements SerialPortDataListener {
+    SerialPort sp;
+    private String message = "";
 
     public RobotCommands(){
         sp = SerialPort.getCommPort("COM5"); // selecteer je gebruikte COM port
         sp.setComPortParameters(9600, 8, 1, 0); //Set Serial baudrate
         sp.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0); //timeouts uitzetten
+
+        /*
+        sp.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                    return;
+                byte[] newData = new byte[sp.bytesAvailable()];
+                int numRead = sp.readBytes(newData, newData.length);
+                System.out.println("Read " + numRead + " bytes.");
+            }
+        });
+         */
+
+        /*
+        sp.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_WRITTEN;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                if (event.getEventType() == SerialPort.LISTENING_EVENT_DATA_WRITTEN)
+                    System.out.println("All bytes were successfully transmitted!");
+            }
+        });
+         */
+
+        sp.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() {
+                return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+            }
+
+            @Override
+            public void serialEvent(SerialPortEvent event) {
+                message += new String(event.getReceivedData());
+                System.out.println(message);
+            }
+        });
+
+    }
+
+
+    @Override
+    public int getListeningEvents() {
+        return 0;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+
+    }
+
+
+
+
+    public void handleResponse(String response) {
+        System.out.println(response);
+    }
+
+    public SerialPort openSP() {
+        sp.openPort();
+        return sp;
     }
 
     public SerialPort getSp() {
         return sp;
     }
 
-    public void moveRobotToLocation(SerialPort sp, String location) throws IOException {
-        byte[] bytes;
-        bytes = location.getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
-        sp.getOutputStream().write(bytes);
-        sp.getOutputStream().flush();
+    public void sendLocation(SerialPort sp, String location) throws IOException {
+            getSp().openPort();
+            byte[] bytes;
+            bytes = location.getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
+            sp.getOutputStream().write(bytes);
+            System.out.println("Send location: " + location);
+
+    }
+
+    public void sendCommandMode(SerialPort sp) throws IOException {
+        getSp().openPort();
+            byte[] bytes;
+            bytes = "COORDS".getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
+            sp.getOutputStream().write(bytes);
     }
 
     public void moveRobotUp(SerialPort sp) throws IOException {
@@ -32,7 +117,6 @@ public class RobotCommands {
             byte[] bytes;
             bytes = "UP".getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
             sp.getOutputStream().write(bytes);
-            sp.getOutputStream().flush();
         }
     }
 
@@ -42,7 +126,6 @@ public class RobotCommands {
             byte[] bytes;
             bytes = "DOWN".getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
             sp.getOutputStream().write(bytes); // stuur hier je bytes
-            sp.getOutputStream().flush();
         }
     }
 
@@ -51,7 +134,6 @@ public class RobotCommands {
             byte[] bytes;
             bytes = "LEFT".getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
             sp.getOutputStream().write(bytes);
-            sp.getOutputStream().flush();
         }
 
 
@@ -63,15 +145,22 @@ public class RobotCommands {
             byte[] bytes;
             bytes = "RIGHT".getBytes(StandardCharsets.UTF_8); //Zet string om naar bytes en stuurt naar arduino
             sp.getOutputStream().write(bytes);
-            sp.getOutputStream().flush();
         }
     }
 
     public SerialPort closeArduinoConnection(){
-        SerialPort sp = SerialPort.getCommPort("COM7");
+        SerialPort sp = SerialPort.getCommPort("COM5");
         if(sp.closePort()) { // sluit de connectie
             System.out.println("Failed to open port :(");
         }
         return sp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
